1138c88aa7cb41ced39896c32c7cf874392e21b5
karen <karen.perez.0395@gmail.com>
2023-02-01 01:50:00 -0300

    tarea realizada

---
+++ upcoming.js
@@ -0,0 +1,20 @@
+
+const cards = document.getElementById("div-cards")
+let info = data.events
+let card = " "
+
+for(let infoEvent of info) {
+    if (infoEvent.date > data.currentDate)
+        card += `<div class="card mb-3 p-2 cover"  style="width: 18rem;">
+        <img src=${infoEvent.image} class="card-img-top h-50" alt="event">
+        <div class="card-body ">
+          <h5 class="card-title h-25">${infoEvent.name}</h5>
+            <p class="card-text h-50">${infoEvent.description}</p>
+          <div class="d-flex justify-content-evenly align-items-center">  
+            <p class="m-0">${infoEvent.price}</p>
+            <a href="./details.html" class="btn btn-outline-danger">Details</a>
+          </div>
+        </div>
+        </div>`
+}
+cards.innerHTML = card
